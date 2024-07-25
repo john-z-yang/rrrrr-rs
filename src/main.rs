a8cac83961e828897d3a12c5f1cb6d2ad5deb75e
John Yang <johnyang@protonmail.com>
2024-07-25 00:50:57 -0700

    wip: add rustyline

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
 #![allow(dead_code)]
+extern crate rustyline;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+use compile::lex::scan;
 mod compile;
 
 fn main() {
-    let lambda_expr = sexpr!(#"lambda", (#"x", #"y"), (#"foo", #"x", #"y"));
-
-    println!("created expression:\n  {}", lambda_expr);
-
-    match_sexpr! {(#"lambda", (args @ ..), body @ ..) = lambda_expr => {
-        println!("matched p1, args {}, body {}", args, body);
-    }};
+    let mut rl = Editor::<()>::new();
+    let _ = rl.load_history("history.txt");
+    let mut lines = String::new();
+    loop {
+        let readline = rl.readline(if lines.is_empty() { "lisp> " } else { " ... " });
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                if line.is_empty() {
+                    println!("{:?}", scan(&lines));
+                    lines.clear();
+                    break;
+                }
+                lines.push_str(&line);
+                lines.push_str("\n");
+            }
+            Err(ReadlineError::Eof) => {
+                println!("Farewell.");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    rl.save_history("history.txt").unwrap();
 }
