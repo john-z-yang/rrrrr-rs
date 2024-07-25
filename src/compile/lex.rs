d48403990bc4fc9cebcd90d1c798093698eabee6
John Yang <johnyang@protonmail.com>
2024-07-25 11:38:25 -0700

    wip: lexer: lint

--- src/compile/lex.rs
+++ src/compile/lex.rs
@@ -3,7 +3,10 @@ use std::{
     str::Chars,
 };
 
-use crate::compile::{sexpr::Num, src_loc::SourceLoc};
+use crate::compile::{
+    sexpr::{Num, Symbol},
+    src_loc::SourceLoc,
+};
 
 use super::{compliation_error::CompliationError, token::Token};
 
@@ -43,10 +46,20 @@ pub fn tokenize(source: &str) -> Result<Vec<Token>, CompliationError> {
                 '\n' => self.line += 1,
                 '(' => self.add_token(Token::LParen(self.get_src_loc())),
                 ')' => self.add_token(Token::RParen(self.get_src_loc())),
-                '\'' => self.add_token(Token::Quote(self.get_src_loc())),
-                '.' => self.add_token(Token::Dot(self.get_src_loc())),
                 '`' => self.add_token(Token::QuasiQuote(self.get_src_loc())),
                 '|' => self.add_token(Token::Pipe(self.get_src_loc())),
+                '\'' => self.add_token(Token::Quote(self.get_src_loc())),
+                '.' => {
+                    if self.advance_if('.') {
+                        if let Some('.') = self.look_ahead() {
+                            self.add_token(Token::Id(Symbol::new("..."), self.get_src_loc()))
+                        } else {
+                            Err(self.emit_err("Expecting '.' after '..'"))?
+                        }
+                    } else {
+                        self.add_token(Token::Dot(self.get_src_loc()))
+                    }
+                }
                 ',' => {
                     if self.advance_if('@') {
                         self.add_token(Token::CommaAt(self.get_src_loc()))
@@ -59,38 +72,42 @@ pub fn tokenize(source: &str) -> Result<Vec<Token>, CompliationError> {
                     .then(|| self.add_token(Token::HashLParen(self.get_src_loc())))
                     .ok_or_else(|| self.emit_err("Expecting '(' after '#'"))?,
                 ';' => {
-                    while let Some(c) = self.look_ahead() {
-                        if c == '\n' {
-                            break;
-                        }
-                        self.advance();
-                    }
+                    self.advance_until(&|c| if c == '\n' { true } else { false });
                 }
                 '0'..='9' | '-' => self.parse_num()?,
                 '"' => self.parse_string()?,
+                c if Self::is_id_initial(c) => self.parse_id()?,
                 c => Err(self.emit_err(&format!("Unexpeted character: '{}'", c)))?,
             };
             Ok(())
         }
-        fn parse_num(&mut self) -> Result<(), CompliationError> {
+        fn parse_id(&mut self) -> Result<(), CompliationError> {
             while let Some(c) = self.look_ahead() {
-                match c {
-                    '0'..='9' => (),
-                    _ => break,
-                };
+                if !Self::is_id_subsequent(c) {
+                    break;
+                }
                 self.advance();
             }
+            self.add_token(Token::Id(
+                Symbol::new(&self.source[self.start..self.cur]),
+                self.get_src_loc(),
+            ));
+            Ok(())
+        }
+        fn parse_num(&mut self) -> Result<(), CompliationError> {
+            self.advance_until(&|c| match c {
+                '0'..='9' => false,
+                _ => true,
+            });
 
             if self.look_ahead() == Some('.') {
                 self.advance();
-                while let Some(c) = self.look_ahead() {
-                    match c {
-                        '0'..='9' => (),
-                        _ => break,
-                    };
-                    self.advance();
-                }
+                self.advance_until(&|c| match c {
+                    '0'..='9' => false,
+                    _ => true,
+                });
             }
+
             let sub_str = self.source[self.start..self.cur].to_string();
             self.add_token(Token::Num(
                 Num(sub_str.parse().map_err(|_| {
@@ -101,15 +118,8 @@ pub fn tokenize(source: &str) -> Result<Vec<Token>, CompliationError> {
             Ok(())
         }
         fn parse_string(&mut self) -> Result<(), CompliationError> {
-            while let Some(c) = self.look_ahead() {
-                match c {
-                    '\n' => self.line += 1,
-                    '"' => break,
-                    _ => (),
-                };
-                self.advance();
-            }
-            if self.it.peek().is_none() {
+            self.advance_until(&|c| if c == '"' { true } else { false });
+            if self.look_ahead().is_none() {
                 Err(self.emit_err("Unterminated string"))?;
             };
             self.advance();
@@ -119,8 +129,18 @@ pub fn tokenize(source: &str) -> Result<Vec<Token>, CompliationError> {
             ));
             Ok(())
         }
-        fn look_ahead(&mut self) -> Option<char> {
-            self.it.peek().map(|(_, c)| c).copied()
+        fn advance_until<F>(&mut self, f: &F)
+        where
+            F: Fn(char) -> bool,
+        {
+            while let Some(c) = self.look_ahead() {
+                match c {
+                    '\n' if !f(c) => self.line += 1,
+                    _ if f(c) => break,
+                    _ => (),
+                }
+                self.advance();
+            }
         }
         fn advance_if(&mut self, c: char) -> bool {
             self.it
@@ -131,12 +151,8 @@ pub fn tokenize(source: &str) -> Result<Vec<Token>, CompliationError> {
                 })
                 .unwrap_or(false)
         }
-        fn get_src_loc(&self) -> SourceLoc {
-            SourceLoc {
-                line: self.line,
-                col: self.start,
-                width: self.cur - self.start,
-            }
+        fn look_ahead(&mut self) -> Option<char> {
+            self.it.peek().map(|(_, c)| c).copied()
         }
         fn advance(&mut self) -> char {
             let (pos, c) = self.it.next().unwrap();
@@ -145,6 +161,42 @@ pub fn tokenize(source: &str) -> Result<Vec<Token>, CompliationError> {
         }
         fn add_token(&mut self, token: Token) {
             self.tokens.push(token);
+        }
+        fn get_src_loc(&self) -> SourceLoc {
+            SourceLoc {
+                line: self.line,
+                col: self.start,
+                width: self.cur - self.start,
+            }
+        }
+        fn is_id_initial(c: char) -> bool {
+            match c {
+                'A'..='Z'
+                | 'a'..='z'
+                | '!'
+                | '$'
+                | '%'
+                | '&'
+                | '*'
+                | '/'
+                | ':'
+                | '<'
+                | '='
+                | '>'
+                | '?'
+                | '^'
+                | '_'
+                | '~'
+                | '+'
+                | '-' => true,
+                _ => false,
+            }
+        }
+        fn is_id_subsequent(c: char) -> bool {
+            match c {
+                '0'..='9' | '+' | '-' | '.' | '@' => true,
+                c => Self::is_id_initial(c),
+            }
         }
         fn emit_err(&self, reason: &str) -> CompliationError {
             CompliationError {
@@ -160,7 +212,12 @@ pub fn tokenize(source: &str) -> Result<Vec<Token>, CompliationError> {
 
 #[cfg(test)]
 mod tests {
-    use crate::compile::{lex::tokenize, sexpr::Num, src_loc::SourceLoc, token::Token};
+    use crate::compile::{
+        lex::tokenize,
+        sexpr::{Num, Symbol},
+        src_loc::SourceLoc,
+        token::Token,
+    };
 
     #[test]
     fn test_tokenize_empty() {
@@ -173,7 +230,8 @@ mod tests {
 ; #
 \"ab\" ; #
 ; #
-\"\" 9.0001 0 -3 -42.00 -100";
+\"\" 9.0001 0 -3 -42.00 -100 some-symbol <=? list->vector ;
+2";
         assert_eq!(
             tokenize(src).unwrap(),
             vec![
@@ -256,6 +314,38 @@ mod tests {
                         line: 4,
                         col: 46,
                         width: 4
+                    }
+                ),
+                Token::Id(
+                    Symbol::new("some-symbol"),
+                    SourceLoc {
+                        line: 4,
+                        col: 51,
+                        width: 11
+                    }
+                ),
+                Token::Id(
+                    Symbol::new("<=?"),
+                    SourceLoc {
+                        line: 4,
+                        col: 63,
+                        width: 3
+                    }
+                ),
+                Token::Id(
+                    Symbol::new("list->vector"),
+                    SourceLoc {
+                        line: 4,
+                        col: 67,
+                        width: 12
+                    }
+                ),
+                Token::Num(
+                    Num(2.0),
+                    SourceLoc {
+                        line: 5,
+                        col: 82,
+                        width: 1
                     }
                 ),
                 Token::EoF()
